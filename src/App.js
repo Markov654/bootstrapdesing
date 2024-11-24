04574b34befeee9622c5b69bd1fd10bc00dacd9c
Markov654 <iliq_markov@abv.bg>
2024-11-24 23:03:24 +0200

    try to chache to vertical

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import logo from "./logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+    <div class="row">
+      <div class="col">
+        <img src={logo}></img>
+      </div>
+      <div class="col-6">
+        mql 
+        bau 
+        bum bum
+      </div>
+      <div class="col">
+        3 of 3
+      </div>
     </div>
+   
+  </div>
   );
 }
 
